99b236ead6f228373cbda372c2aa34f18f1c8901
rungjae <sorrylee0907@nate.com>
2022-01-26 17:56:59 +0900

    재성이 3번째

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -15,7 +15,7 @@ public class DemoController {
     //@RequestMapping(value = "/demo" , method = RequestMethod.GET)
     @GetMapping(value = "/demo")  // 인터넷 접속방법!
     public ModelAndView demo(ModelAndView mv, String id){
-        log.info("렁재렁재렁잴어잴저래어래 ㅁㄴㅇㄴㅁㅇㅁㄴㅇㄴㅁㅇㅁ");
+        log.info("렁재세번째 테스트");
 
         String name = "";
         if(id.equals("liu")){
